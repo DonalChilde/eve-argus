72183f5a5e65776a6c55d2075ffd1cc7618738c2
Chad Lowe <pfmsoft.dev@gmail.com>
2025-07-04 01:42:00 -0700

    test and time multiple esi calls

--- scripts/preston-test.py
+++ scripts/preston-test.py
@@ -5,11 +5,24 @@
 # ]
 # ///
 import preston
+from time import perf_counter
+type_ids = [34, 35, 36, 37, 38, 39]
 
 def main() -> None:
+    start=perf_counter()
     p = preston.Preston(useragent="preston-test")
-    data = p.get_op('get_markets_region_id_history', region_id=10000002, type_id=34)
-    print(data)
+    for type_id in type_ids:
+        print(f"Requesting type_id {type_id}")
+        # Get the market history for the given type_id in the Forge region (10000002)
+        # This will use the cached response if available
+        
+        data = p.get_op('get_markets_region_id_history', region_id=10000002, type_id=type_id)
+        print(p.stored_headers[0])
+        print("\n")
+    end = perf_counter()
+    elapsed = end - start
+    print(f"Request took {elapsed:.6f} seconds")
+    
 
 if __name__ == "__main__":
     main()
--- scripts/requests-test.py
+++ scripts/requests-test.py
@@ -5,16 +5,26 @@
 # ]
 # ///
 import requests
-
+from time import perf_counter
+type_ids = [34, 35, 36, 37, 38, 39]
 # The Forge region ID 10000002
 def main() -> None:
-    payload = {
+    start = perf_counter()
+    for type_id in type_ids:
+        print(f"Requesting type_id {type_id}")
+        # Get the market history for the given type_id in the Forge region (10000002)
+        
+        payload = {
         "datasource": 'tranquility',
-        "type_id": 34,
+        "type_id": type_id,
     }
-    r = requests.get("https://esi.evetech.net/latest/markets/10000002/history/", params=payload)
-    print(r)
-    print(r.json())
+        r = requests.get("https://esi.evetech.net/latest/markets/10000002/history/", params=payload)
+        print(payload,r)
+    end = perf_counter()
+    elapsed = end - start
+    print(f"Request took {elapsed:.6f} seconds")
+    
+    # print(r.json())
 
 
 if __name__ == "__main__":
