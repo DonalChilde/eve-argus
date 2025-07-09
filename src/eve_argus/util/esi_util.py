ca1c4f4cb1f899bfbe6fd07835ce94a7d50bb0f5
Chad Lowe <pfmsoft@gmail.com>
2025-07-09 11:58:35 -0700

    refoactor names and use pydantic models

--- src/eve_argus/util/esi_util.py
+++ src/eve_argus/util/esi_util.py
@@ -1,34 +1,36 @@
 from pathlib import Path
 from typing import Any
+from collections.abc import Iterable, Sequence
 from eve_argus.models import esi_data as ED
 from eve_argus.models.market_history_summary import MarketHistorySummary
 import csv
 from datetime import date
 from eve_argus.snippets.datetime.date_range import date_range_days
+from pydantic import BaseModel
 
 
-def market_history_summary(
+def summarize_market_history_by_periods(
     region_id: int, type_id: int, periods: list[int], data: list[ED.MarketHistory]
-) -> dict[int, MarketHistorySummary]:
+) -> Sequence[MarketHistorySummary]:
     lookup = {date.fromisoformat(x["date"]): x for x in data}
-    result: dict[int, MarketHistorySummary] = {}
+    result: list[MarketHistorySummary] = []
     keys = list(lookup.keys())
     keys.sort(reverse=True)  # Sort by date descending
     most_recent = keys[0]
     for period in periods:
         dates = list(date_range_days(start_date=most_recent, days=period, past=True))
         end = dates[-1]
-        summary = generate_history_summary(dates=dates, data=lookup)
-        summary["region_id"] = region_id
-        summary["type_id"] = type_id
-        summary["period"] = period
-        summary["start"] = most_recent.isoformat()
-        summary["end"] = end.isoformat()
-        result[period] = summary
+        summary = summarize_market_history_by_dates(dates=dates, data=lookup)
+        summary.region_id = region_id
+        summary.type_id = type_id
+        summary.period = period
+        summary.start = most_recent.isoformat()
+        summary.end = end.isoformat()
+        result.append(summary)
     return result
 
 
-def generate_history_summary(
+def summarize_market_history_by_dates(
     dates: list[date], data: dict[date, ED.MarketHistory]
 ) -> MarketHistorySummary:
     missing = average = highest = lowest = order_count = volume = 0
@@ -59,24 +61,57 @@ def generate_history_summary(
     return result
 
 
-def market_history_save_to_csv(
-    region_id: int, type_id: int, data: list[ED.MarketHistory], dirpath: Path
-) -> None:
-    """Save market history data to a CSV file.
+# def market_history_save_to_csv(
+#     region_id: int, type_id: int, data: list[ED.MarketHistory], dirpath: Path
+# ) -> None:
+#     """Save market history data to a CSV file.
 
-    Args:
-        region_id (int): The ID of the region.
-        type_id (int): The ID of the type.
-        data (list[ED.MarketHistory]): List of market history data.
-        dirpath (Path): The Path of the directory to save the data to.
-    """
-    filename = f"{region_id}_{type_id}_market_history.csv"
-    filepath = dirpath / filename
-    filepath.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
-    with filepath.open("w", encoding="utf-8") as file:
-        writer = csv.DictWriter(
-            file, fieldnames=ED.MarketHistory.__annotations__.keys()
-        )
-        writer.writeheader()
-        for entry in data:
-            writer.writerow(entry)
+#     Args:
+#         region_id (int): The ID of the region.
+#         type_id (int): The ID of the type.
+#         data (list[ED.MarketHistory]): List of market history data.
+#         dirpath (Path): The Path of the directory to save the data to.
+#     """
+#     filename = f"{region_id}_{type_id}_market_history.csv"
+#     filepath = dirpath / filename
+#     filepath.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+#     with filepath.open("w", encoding="utf-8") as file:
+#         writer = csv.DictWriter(
+#             file, fieldnames=ED.MarketHistory.__annotations__.keys()
+#         )
+#         writer.writeheader()
+#         for entry in data:
+#             writer.writerow(entry)
+
+
+# def pydantic_basemodel_to_csv(data: Iterable[BaseModel], filepath: Path) -> int:
+#     filepath.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+#     data_iterator = iter(data)
+#     count = 0
+#     try:
+#         first_item = next(data_iterator)
+#         count += 1
+#     except StopIteration:
+#         return count
+#     with filepath.open("w", encoding="utf-8") as file_out:
+#         writer = csv.DictWriter(file_out,fieldnames=first_item.)
+
+
+# def market_history_summary_save_to_csv(
+#     data: Iterable[MarketHistorySummary], filepath: Path
+# ) -> None:
+#     """Save market history summary data to a CSV file.
+
+#     Args:
+#         data (Sequence[MarketHistorySummary]): List of market history data.
+#         filepath (Path): The Path of the directory to save the data to.
+#     """
+#     filepath.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+#     with filepath.open("w", encoding="utf-8") as file:
+#         # TODO write this as a general use Pydantic to csv function, top level only?
+#         writer = csv.DictWriter(
+#             file, fieldnames=ED.MarketHistory.__annotations__.keys()
+#         )
+#         writer.writeheader()
+#         for entry in data:
+#             writer.writerow(entry)
