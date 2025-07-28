d97affa6ef8ac5cac2ee781338e048fe21557d12
Chad Lowe <pfmsoft.dev@gmail.com>
2025-07-28 10:49:05 -0700

    move sde loading functions to data_import package.

---
+++ src/eve_argus/data_import/sde_loader.py
@@ -0,0 +1,80 @@
+"""Entry point for SDE data imports."""
+
+import logging
+from pathlib import Path
+from typing import Any
+
+from yaml import safe_load
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+class SdeFilePaths:
+    TYPES = Path("fsd") / "types.yaml"
+    BLUEPRINTS = Path("fsd") / "blueprints.yaml"
+    MARKET_GROUPS = Path("fsd") / "marketGroups.yaml"
+    META_GROUPS = Path("fsd") / "metaGroups.yaml"
+    GROUPS = Path("fsd") / "groups.yaml"
+    CATEGORIES = Path("fsd") / "categories.yaml"
+
+
+class SdeLoader:
+    def __init__(self, sde_path: Path) -> None:
+        """API to load files from the sde.
+
+        Args:
+            sde_path (Path): _description_
+        """
+        self.sde_path = sde_path
+
+    def load_types(self) -> dict[int, dict[str, Any]]:
+        """Load type info from the sde.
+
+        Returns:
+            dict[int, dict[str, Any]]: _description_
+        """
+        path_in = self.sde_path / SdeFilePaths.TYPES
+        with open(path_in) as file_in:
+            data = safe_load(file_in)
+        return data
+
+    def load_blueprints(self) -> dict[int, dict[str, Any]]:
+        """Load blueprints from the sde.
+
+        Returns:
+            dict[int, dict[str, Any]]: _description_
+        """
+        path_in = self.sde_path / SdeFilePaths.BLUEPRINTS
+        with open(path_in) as file_in:
+            data = safe_load(file_in)
+        return data
+
+    def load_market_groups(self) -> dict[int, dict[str, Any]]:
+        """Load market groups from sde.
+
+        Returns:
+            dict[int, dict[str, Any]]: _description_
+        """
+        path_in = self.sde_path / SdeFilePaths.MARKET_GROUPS
+        with open(path_in) as file_in:
+            data = safe_load(file_in)
+        return data
+
+    def load_meta_groups(self) -> dict[int, dict[str, Any]]:
+        path_in = self.sde_path / SdeFilePaths.META_GROUPS
+        with open(path_in) as file_in:
+            data = safe_load(file_in)
+        return data
+
+    def load_groups(self) -> dict[int, dict[str, Any]]:
+        path_in = self.sde_path / SdeFilePaths.GROUPS
+        with open(path_in) as file_in:
+            data = safe_load(file_in)
+        return data
+
+    def load_categories(self) -> dict[int, dict[str, Any]]:
+        path_in = self.sde_path / SdeFilePaths.CATEGORIES
+        with open(path_in) as file_in:
+            data = safe_load(file_in)
+        return data
--- src/eve_argus/file_loader.py
+++ src/eve_argus/file_loader.py
@@ -6,9 +6,6 @@ from collections.abc import Iterable
 from pathlib import Path
 from string import Template
 from time import perf_counter
-from typing import Any
-
-from yaml import safe_load
 
 from eve_argus.models import argus as EAM
 from eve_argus.snippets.file.csv import write_dicts_to_csv
@@ -16,76 +13,6 @@ from eve_argus.snippets.file.validate_file_out import validate_file_out
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
-
-
-class SdeFilePaths:
-    TYPES = Path("fsd") / "types.yaml"
-    BLUEPRINTS = Path("fsd") / "blueprints.yaml"
-    MARKET_GROUPS = Path("fsd") / "marketGroups.yaml"
-    META_GROUPS = Path("fsd") / "metaGroups.yaml"
-    GROUPS = Path("fsd") / "groups.yaml"
-    CATEGORIES = Path("fsd") / "categories.yaml"
-
-
-class SdeLoader:
-    def __init__(self, sde_path: Path) -> None:
-        """API to load files from the sde.
-
-        Args:
-            sde_path (Path): _description_
-        """
-        self.sde_path = sde_path
-
-    def load_types(self) -> dict[int, dict[str, Any]]:
-        """Load type info from the sde.
-
-        Returns:
-            dict[int, dict[str, Any]]: _description_
-        """
-        path_in = self.sde_path / SdeFilePaths.TYPES
-        with open(path_in) as file_in:
-            data = safe_load(file_in)
-        return data
-
-    def load_blueprints(self) -> dict[int, dict[str, Any]]:
-        """Load blueprints from the sde.
-
-        Returns:
-            dict[int, dict[str, Any]]: _description_
-        """
-        path_in = self.sde_path / SdeFilePaths.BLUEPRINTS
-        with open(path_in) as file_in:
-            data = safe_load(file_in)
-        return data
-
-    def load_market_groups(self) -> dict[int, dict[str, Any]]:
-        """Load market groups from sde.
-
-        Returns:
-            dict[int, dict[str, Any]]: _description_
-        """
-        path_in = self.sde_path / SdeFilePaths.MARKET_GROUPS
-        with open(path_in) as file_in:
-            data = safe_load(file_in)
-        return data
-
-    def load_meta_groups(self) -> dict[int, dict[str, Any]]:
-        path_in = self.sde_path / SdeFilePaths.META_GROUPS
-        with open(path_in) as file_in:
-            data = safe_load(file_in)
-        return data
-
-    def load_groups(self) -> dict[int, dict[str, Any]]:
-        path_in = self.sde_path / SdeFilePaths.GROUPS
-        with open(path_in) as file_in:
-            data = safe_load(file_in)
-        return data
-
-    def load_categories(self) -> dict[int, dict[str, Any]]:
-        path_in = self.sde_path / SdeFilePaths.CATEGORIES
-        with open(path_in) as file_in:
-            data = safe_load(file_in)
-        return data
 
 
 class ArgusFilePaths:
