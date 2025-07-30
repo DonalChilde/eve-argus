30d20548285a856b20ee79c3a9267d2520b9d26e
Chad Lowe <pfmsoft.dev@gmail.com>
2025-07-30 08:07:52 -0700

    stubs for argus filebased database.

---
+++ src/eve_argus/file_data/__init__.py
@@ -0,0 +1,1 @@
+"""Code surrounding the use of the filesystem to store eve-argus data."""
---
+++ src/eve_argus/file_data/argus_file_database.py
@@ -0,0 +1,7 @@
+"""The API for the argus filebased database."""
+
+# File based data point of entry
+# lazy loading of data files
+# Stores data from opened files for use.
+# check if files are present, and if data is expired.
+# report currency of all data
