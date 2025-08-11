ece2f0d1cb3312b9a726e84083563e289ae74af6
Chad Lowe <pfmsoft.dev@gmail.com>
2025-08-11 14:56:44 -0700

    deepcopy cached values to avoid mutable object issues.

--- src/eve_argus/esi_client/argus_aiohttp/esi_cache.py
+++ src/eve_argus/esi_client/argus_aiohttp/esi_cache.py
@@ -1,7 +1,13 @@
 """Cache for Esi get requests."""
 
+import logging
+from copy import deepcopy
+
 from eve_argus.esi_client.esi_client_protocol import EsiCacheProtocol
 from eve_argus.models.esi import EsiResponse
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 class EsiMemoryCache(EsiCacheProtocol):
@@ -12,11 +18,16 @@ class EsiMemoryCache(EsiCacheProtocol):
 
     def get(self, key: str) -> EsiResponse | None:
         """Retrieve a cached response by its key."""
-        return self._cache.get(key)
+        cached_value = self._cache.get(key)
+        if cached_value:
+            logger.info(f"Cache hit for key: {key}")
+        return cached_value
 
     def set(self, key: str, value: EsiResponse) -> None:
         """Store a response in the cache with its key."""
-        self._cache[key] = value
+        copied_value = deepcopy(value)
+        copied_value.source = "cache"
+        self._cache[key] = copied_value
 
     def clear(self) -> None:
         """Clear the entire cache."""
