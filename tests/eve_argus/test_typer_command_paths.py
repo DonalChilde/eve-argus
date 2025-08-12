69d0b29e892d2c1f86b56b577bda6dd6466c1426
Chad Lowe <pfmsoft.dev@gmail.com>
2025-08-12 08:10:00 -0700

    remove obsolete tests

--- tests/eve_argus/test_typer_command_paths.py
+++ tests/eve_argus/test_typer_command_paths.py
@@ -1,4 +1,5 @@
 """Test cases for the console module."""
+
 from importlib import resources
 
 import pytest
@@ -10,6 +11,7 @@ from tests.resources import RESOURCES_ANCHOR
 DATA_FILE_NAME = "ipsum_1.txt"
 DATA_FILE_PATH = "files_1"
 DATA_FILE_ANCHOR = f"{DATA_FILE_PATH}/{DATA_FILE_NAME}"
+
 
 @pytest.fixture
 def runner() -> CliRunner:
@@ -25,25 +27,23 @@ def test_app(runner: CliRunner) -> None:
         print(result.stderr)
     assert result.exit_code == 0
 
-def test_hash_md5(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(app, ["hash-md5", "--help"])
-    print(result.stdout)
-    if result.stderr_bytes is not None:
-        print(result.stderr)
-    assert result.exit_code == 0
 
-def test_default_options(runner:CliRunner)-> None:
-    file_resource = resources.files(RESOURCES_ANCHOR).joinpath(DATA_FILE_ANCHOR)
-    with resources.as_file(file_resource) as input_path:
-        result = runner.invoke(
-            app, ["-vvv", "hash-md5", str(input_path)]
-        )
-        assert "Verbosity: 3" in result.stdout
-        print(result.stdout)
-        if result.stderr_bytes is not None:
-            print(result.stderr)
-        assert result.exit_code == 0
+# def test_hash_md5(runner: CliRunner) -> None:
+#     """It exits with a status code of zero."""
+#     result = runner.invoke(app, ["hash-md5", "--help"])
+#     print(result.stdout)
+#     if result.stderr_bytes is not None:
+#         print(result.stderr)
+#     assert result.exit_code == 0
 
-
-
+# def test_default_options(runner:CliRunner)-> None:
+#     file_resource = resources.files(RESOURCES_ANCHOR).joinpath(DATA_FILE_ANCHOR)
+#     with resources.as_file(file_resource) as input_path:
+#         result = runner.invoke(
+#             app, ["-vvv", "hash-md5", str(input_path)]
+#         )
+#         assert "Verbosity: 3" in result.stdout
+#         print(result.stdout)
+#         if result.stderr_bytes is not None:
+#             print(result.stderr)
+#         assert result.exit_code == 0
