f770d78cd2c18978a1cecf084912d7004b0ee270
Chad Lowe <pfmsoft.dev@gmail.com>
2025-11-04 10:09:23 -0700

    simple_download_async helper

---
+++ src/eve_argus/helpers/simple_download_async.py
@@ -0,0 +1,122 @@
+"""Simple functions to download from a web server."""
+
+import logging
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from time import perf_counter
+from typing import Any
+
+import aiohttp
+
+from eve_argus.helpers.expand_multidict import ExpandedHeaders, expand_multi_dict
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+type SimpleText = tuple[str, ExpandedHeaders]
+type SimpleJSON = tuple[Any, ExpandedHeaders]
+
+
+async def download_file(
+    url: str,
+    *,
+    headers: dict[str, str],
+    session: aiohttp.ClientSession | None,
+    file_path: Path,
+    overwrite: bool = False,
+) -> ExpandedHeaders:
+    """Download a file from a URL and save it to disk.
+
+    Args:
+        url: The URL to download the file from.
+        headers: The headers to include in the request.
+        session: An optional aiohttp ClientSession to use for the request.
+        file_path: The path to save the downloaded file to.
+        overwrite: Whether to overwrite the file if it already exists.
+
+    Returns:
+        The expanded headers from the response.
+    """
+    logger.info(f"Downloading file from {url}")
+    if file_path.is_dir():
+        raise IsADirectoryError(f"File path {file_path} is a directory.")
+    if file_path.exists() and not overwrite:
+        logger.info(f"File {file_path} already exists and overwrite is False. {url=}")
+        raise FileExistsError(
+            f"File {file_path} already exists and overwrite is False."
+        )
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    start = perf_counter()
+    if session is None:
+        session = aiohttp.ClientSession()
+    async with session.get(url, headers=headers) as response:
+        logger.debug(
+            "Received response with status %s, reason %s from %s, headers=%r",
+            response.status,
+            response.reason,
+            response.real_url,
+            response.headers,
+        )
+        response.raise_for_status()
+        # atomic write to temp file, then move to final location
+        with NamedTemporaryFile() as tmp_file:
+            tmp_file.write(await response.read())
+            tmp_file.flush()
+            tmp_path = Path(tmp_file.name)
+            tmp_path.replace(file_path)
+
+        logger.info(
+            f"Downloaded and wrote file from {url} to {file_path} in {perf_counter() - start:.2f} seconds"
+        )
+        expanded_headers = expand_multi_dict(response.headers)  # type: ignore
+        return expanded_headers
+
+
+async def download_text(
+    url: str, *, headers: dict[str, str], session: aiohttp.ClientSession | None
+) -> SimpleText:
+    """Download a text file from a URL and return its content as a string."""
+    logger.info(f"Downloading text from {url}")
+    start = perf_counter()
+    if session is None:
+        session = aiohttp.ClientSession()
+    async with session.get(url, headers=headers) as response:
+        logger.debug(
+            "Received response with status %s, reason %s from %s, headers=%r",
+            response.status,
+            response.reason,
+            response.real_url,
+            response.headers,
+        )
+        response.raise_for_status()
+        text = await response.text()
+        logger.info(
+            f"Downloaded text from {url} in {perf_counter() - start:.2f} seconds"
+        )
+        result_headers = expand_multi_dict(response.headers)  # type: ignore
+        return (text, result_headers)
+
+
+async def download_json(
+    url: str, *, headers: dict[str, str], session: aiohttp.ClientSession | None
+) -> SimpleJSON:
+    """Download JSON data from a URL."""
+    logger.info(f"Downloading JSON from {url}")
+    start = perf_counter()
+    if session is None:
+        session = aiohttp.ClientSession()
+    async with session.get(url, headers=headers) as response:
+        logger.debug(
+            "Received response with status %s, reason %s from %s, headers=%r",
+            response.status,
+            response.reason,
+            response.real_url,
+            response.headers,
+        )
+        response.raise_for_status()
+        json_data = await response.json()
+        logger.info(
+            f"Downloaded json from {url} in {perf_counter() - start:.2f} seconds"
+        )
+        result_headers = expand_multi_dict(response.headers)  # type: ignore
+        return (json_data, result_headers)
